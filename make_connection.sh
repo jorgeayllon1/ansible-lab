5838302d55c1246bdfd55859ff9c4cb260b7416c
Jorge Ayllon <jorge2640@hotmail.Fr>
2023-09-22 20:09:55 +0000

    Fix #1 Ajout du script en bash

---
+++ make_connection.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+FILE_KEY=~/.sh/id_rsa_ansible.pub
+user=$1
+remote_address=$2
+
+if [ ! -f "$FILE" ]; then
+    ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa_ansible >/dev/null 2>&1
+fi
+
+ssh-copy-id -i ~/.ssh/id_rsa_ansible.pub $user@$remote_address
