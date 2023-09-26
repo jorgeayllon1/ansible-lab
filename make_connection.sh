f729f17e442c8acddbfe85fcc2087e8cb9054f11
Jorge Ayllon <jorge2640@hotmail.Fr>
2023-09-26 21:14:17 +0200

    Fix #12 Ajout de ssh-copy en automatique

--- make_connection.sh
+++ make_connection.sh
@@ -8,4 +8,11 @@ if ! [ -e "$FILE_KEY" ]; then
     ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa_ansible >/dev/null 2>&1
 fi
 
-ssh-copy-id -i ~/.ssh/id_rsa_ansible.pub $user@$remote_address
+ssh -q -o BatchMode=yes -i ~/.ssh/id_rsa_ansible ${user}@${remote_address} exit
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+    ssh-copy-id -i ~/.ssh/id_rsa_ansible.pub ${user}@${remote_address}
+    ssh -i ~/.ssh/id_rsa_ansible ${user}@${remote_address}
+else
+    ssh -i ~/.ssh/id_rsa_ansible ${user}@${remote_address}
+fi
