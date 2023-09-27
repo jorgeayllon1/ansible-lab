47dbab37e6e08df46a8e74b29e6760a56e5a5b02
Jorge Ayllon <jorge2640@hotmail.Fr>
2023-09-27 22:48:34 +0200

    Fix #13 Ajout du mot de passe + utilisation d'un meilleur algo de chiffrement.

--- make_connection.sh
+++ make_connection.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-FILE_KEY=~/.ssh/id_rsa_ansible.pub
+FILE_KEY=~/.ssh/id_ed25519_ansible.pub
 user=$1
 remote_address=$2
+password=$3
 
 if ! [ -e "$FILE_KEY" ]; then
-    ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa_ansible >/dev/null 2>&1
+    ssh-keygen -q -t ed25519 -b 4096 -N '' -f ~/.ssh/id_ed25519_ansible >/dev/null 2>&1
 fi
 
-ssh -q -o BatchMode=yes -i ~/.ssh/id_rsa_ansible ${user}@${remote_address} exit
+ssh -q -o BatchMode=yes -i ~/.ssh/id_ed25519_ansible ${user}@${remote_address} exit
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
-    ssh-copy-id -i ~/.ssh/id_rsa_ansible.pub ${user}@${remote_address}
-    ssh -i ~/.ssh/id_rsa_ansible ${user}@${remote_address}
+    ssh-keyscan ${remote_address} >> ~/.ssh/known_hosts 2>/dev/null
+    sshpass -p ${password} ssh-copy-id -i ~/.ssh/id_ed25519_ansible.pub ${user}@${remote_address} 2>/dev/null
+    sshpass -p ${password} ssh -i ~/.ssh/id_ed25519_ansible ${user}@${remote_address} 2>/dev/null
 else
-    ssh -i ~/.ssh/id_rsa_ansible ${user}@${remote_address}
+    sshpass -p ${password} ssh -i ~/.ssh/id_ed25519_ansible ${user}@${remote_address} 2>/dev/null
 fi
